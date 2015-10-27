0bbdd871b98b431deebbd76f3ce3203face8dde7
Daniel Munoz <lostlucidity@gmail.com>
2015-10-26 20:21:01 -0400

    added profile model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026015858) do
+ActiveRecord::Schema.define(version: 20151026234239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20151026015858) do
 
   add_index "archives", ["user_id"], name: "index_archives_on_user_id", using: :btree
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
@@ -42,4 +51,5 @@ ActiveRecord::Schema.define(version: 20151026015858) do
   add_index "users", ["name"], name: "index_users_on_name", using: :btree
 
   add_foreign_key "archives", "users"
+  add_foreign_key "profiles", "users"
 end
