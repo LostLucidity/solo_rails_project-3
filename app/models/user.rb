34fdc0dbc8b59f5b2fdeca33cf9dbfc15d6869fb
Daniel Munoz <lostlucidity@gmail.com>
2015-10-27 23:08:27 -0400

    added password digest to users model for secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :archives
 	has_one :profile
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026234239) do
+ActiveRecord::Schema.define(version: 20151028030237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,11 @@ ActiveRecord::Schema.define(version: 20151026234239) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
-    t.string   "email",      null: false
+    t.string   "email",           null: false
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
