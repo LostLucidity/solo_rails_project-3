abc2bd6197714a9d24a7561d681214e41a51b38f
Daniel Munoz <lostlucidity@gmail.com>
2015-11-14 15:51:10 -0500

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'users/new'
 
   # The “root” method maps “/” (the root route) to a 'controller#action'. 
